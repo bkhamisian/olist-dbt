ed6362bcaec1872b57c74f0cc482299106b61a44
bkhamisian <bkhamisian@gmail.com>
2025-03-20 15:52:23 +0400

    Semantic and Metrics Layer v1.2

--- models/marts/marts_products.sql
+++ models/marts/marts_products.sql
@@ -7,7 +7,8 @@ select
     product_id,
     product_category_name,
     product_category_name_english,
-    count(order_id)                 as total_orders,
+    min(order_purchase_date)        as product_first_sold_date,
+    count(order_id)                 as total_product_orders,
     sum(price)                      as total_revenue,
     round(avg(price), 2)            as avg_price
 from order_items
