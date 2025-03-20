530fc991b7395c9eecdc9737419a3d35f51abbef
bkhamisian <bkhamisian@gmail.com>
2025-03-20 20:36:53 +0400

    Documenting Intermediate Models

--- models/intermediate/int_order_payments.sql
+++ models/intermediate/int_order_payments.sql
@@ -7,7 +7,6 @@ select
     order_id,
     count(distinct payment_type) as cnt_payment_types,
     sum(payment_value)           as total_payment_value,
-    count(payment_installments)  as total_installments,
-    count(payment_sequential)    as total_payment_sequential
+    count(payment_installments)  as total_installments
 from payments
 group by order_id
